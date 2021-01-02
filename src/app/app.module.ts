32f69646f0a4f9895d9761fcdc1c554d126bf6d7
joseramirez013 <joseramirez013@hotmail.com>
2021-01-02 13:26:35 -0600

    Master page, footer y componentes
    Master page, footer y componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './public/home/default/default.component';
+import { NavbarComponent } from './public/master-page/navbar/navbar.component';
+import { SideMenuComponent } from './public/master-page/side-menu/side-menu.component';
+import { HeroComponent } from './public/master-page/hero/hero.component';
+import { FooterComponent } from './public/master-page/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    NavbarComponent,
+    SideMenuComponent,
+    HeroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
